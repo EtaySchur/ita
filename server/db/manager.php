d9e4b01542740ff403304af9596cc1ab4ecf5b55
Etay <etayschur@gmail.com>
2016-10-23 17:22:12 +0300

    sticky navbar and fix text vertical

--- server/db/manager.php
+++ server/db/manager.php
@@ -100,7 +100,7 @@ class DbManager {
 
     public static function getProjects(){
         $conn = self::connectToDb();
-        $sql = $conn->prepare("SELECT * from `projects` WHERE `isDeleted` = 0 AND `published` = 1 ORDER BY itemOrder ASC");
+        $sql = $conn->prepare("SELECT * from `projects` WHERE `isDeleted` = 0 AND `published` = 1 ORDER BY itemOrder * 1 ASC");
         $sql->execute();
         $result = $sql->fetchAll(PDO::FETCH_ASSOC);
         return $result;
